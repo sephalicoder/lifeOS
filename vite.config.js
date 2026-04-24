c035593839024911476d5454222c7d4de8bff1ad
Sephali <sephali.adu.info@gmal.com>
2026-04-24 12:53:41 +0530

    Fix case sensitivity in base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lifeos/', // Change this to your GitHub repo name
+  base: '/lifeOS/', // Change this to your GitHub repo name
 })
