2b031e5a5393f4c07fbb82a10e9de314200fa015
sephalicoder <sephali.adu.info@gmail.com>
2026-07-21 12:05:41 +0530

    Fix Vite base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/lifeOS/', // Change this to your GitHub repo name
+  base: '/',
 })
